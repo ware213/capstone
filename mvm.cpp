597e8e9356d1f3a9b1aea04185bbdcf7648017e3
Vince Ware <ware.v.k@gmail.com>
2016-05-06 08:48:02 -0400

    New Dynamic Computations in mvm and a, part of new script in capstone.m

--- mvm.cpp
+++ mvm.cpp
@@ -233,6 +233,14 @@ void script_out(ofstream &fout, double** a, int n, int m)
   }
   fout << "];" << endl;
 
+  for(int i=0; i<n; i++)
+  {
+    for(int j=0; j<m; j++)
+    {
+      // TODO
+    }
+  }
+
   // TODO
 
 
